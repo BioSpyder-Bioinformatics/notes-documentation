ff45b89b85d19a8596c7916e38aa45243c76ac98
Gioele-M <gioelemontis97@gmail.com>
2023-01-24 19:20:13 +0000

    functions and dictionaries for sample sheet

--- sample_sheet_code/sample_sheet_functions.py
+++ sample_sheet_code/sample_sheet_functions.py
@@ -3,21 +3,23 @@ from dictionaries import *
 import pandas as pd
 ##################### Make template
 
-# 24 well plate
-# upload everithn
-
-
+# 24 well plate and
 # 48 well plate
 
 
 
-# 96 well plate
+
+
+
+# 96 well plate(s)
 import os
 
-# Import dictionary only once so to don't have to re-process at each call
-standard_96_well_table = pd.DataFrame.from_dict(standard_96_dict)
+
 # Function requires filename and the number of plates to be concatenated (1<x<8)
 def make_96_well_sample_sheet(filename, n_plates):
+    # Load table in df format
+    table_96_well = pd.DataFrame.from_dict(different_96_dict)
+
     # n_plates has to be 1 < x < 8
     if n_plates < 1 or n_plates > 8:
         raise Exception('Cannot process that quantity')
@@ -29,7 +31,7 @@ def make_96_well_sample_sheet(filename, n_plates):
     # Open file once and append tables to it. The file is opened in 'w' so to over-write anything else (anyway you get blocked by the exception above if the file already exists)
     with open(filename, 'w') as file:
         for _ in range(n_plates):
-            standard_96_well_table.to_csv(file, mode='a', index=True, header=True)
+            table_96_well.to_csv(file, mode='a', index=True, header=True)
     
     # In dash we return the file instance??
 
@@ -51,9 +53,23 @@ def make_96_well_sample_sheet(filename, n_plates):
 # Function recognising type of plate and if 96 well plate split in many DFs
 #_ requires recognising the type of plate
 
+# Check shape of DF
 
 
+#
 
+
+# Function to get 96-plates (even if many • doesn't enforce-check if it's doing it right)
+def get_tables(sample_sheet):
+    # Number of rows in sample sheet
+    df_len = len(sample_sheet)
+    #dfs to be
+    dfs = []
+    # Slice the sample sheet so that the recurring 10th line gets skipped (the empty one)
+    for x in range(8, df_len+1, 10):
+        low_end = 0 if x == 8 else x - 8
+        dfs.append(sample_sheet.iloc[low_end:x, :])
+    return dfs
 
 # Function for enforcing naming restrictions on samples
 
