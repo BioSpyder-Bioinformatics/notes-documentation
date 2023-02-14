f5634feceb0198bdf2fdda552b92dc4411aee086
Gioele-M <gioelemontis97@gmail.com>
2023-02-14 12:42:01 +0000

    checkpoint adapted new functions

--- sample_sheet_code/dash_sample_sheet_functions.py
+++ sample_sheet_code/dash_sample_sheet_functions.py
@@ -2,7 +2,7 @@ import pandas as pd
 from io import StringIO
 from base64 import b64decode
 import re
-from dictionaries import plate_wells_mapping, complete_sample_sheet_string
+from dictionaries import *
 
 ####### HELPER FUNCTIONS
 
@@ -174,9 +174,212 @@ def get_sample_sheet_body(tables, complete):
 
 
 
+# Helper function to manage the adding of the 24/48 wells plates to the main sample sheet
+def add_tf_fe(sample_sheet, tuple, reversed):
+    flavour, table = tuple
+
+    # Map 96 well plate to sample sheet 
+    mapped_plate = map_sample_to_well(table, flavour)
+    
+    # Remove blanks from mapping
+    clean_plate = mapped_plate.drop(mapped_plate[mapped_plate['Sample_Name'] == 'blank'].index).reset_index(drop=True)
+
+    # Map plate to indexes and get all the generic info needed
+    dictionary = flavours_list[flavour]
+    
+    to_add = ["I7_Index_ID","index","I5_Index_ID","index2","index2_reverse"]
+    for key in to_add:
+        clean_plate[key] = clean_plate['Sample_Well'].map(dictionary[key])
+
+    # If reversed drop the column index2 and rename index2_reverse to index2
+    # Else drop index2_reverse
+
+    if reversed:
+        print('reversed')
+        clean_plate = clean_plate.drop(columns=['index2'])
+        clean_plate = clean_plate.rename(columns={'index2_reverse': 'index2'})
+        print(clean_plate.keys())
+
+    else:
+        clean_plate.drop(columns=['index2_reverse'], inplace=True)
+
+    # Declare missing columns empty eg Sample_ID, Sample_Project and Description
+    clean_plate['Sample_ID'], clean_plate['Sample_Project'], clean_plate['Description'], = '','',''
+
+    # Order columns as per sample sheet
+    clean_plate = clean_plate[["Sample_ID","Sample_Name","Sample_Plate","Sample_Well","I7_Index_ID","index","I5_Index_ID","index2","Sample_Project","Description"]]
+
+    # Merge DFs and reset indexes
+    merged = pd.concat([sample_sheet, clean_plate])
+    merged.reset_index(drop=True, inplace=True)
+    merged["Sample_ID"] = merged.index + 1
+
+    return merged
+
+
+
 
 
 
 
 ##########################
 ######## WRAPPER FUNCTIONS
+
+
+# Wrapper to produce the template based on selection, and metadata, returns filename and sample sheet to be written 
+def wrapper_make_template(selection, project_name, experiment_name, additional_comments, file_format='csv'):
+    """
+    Wrapper to produce the template based on selection, and metadata, returns filename and sample sheet to be written 
+    """
+
+    sample_sheet = ''
+    filename = ''
+
+    # Iterate through the selection, if a key is true, add it to the sample sheet.
+    # If the key is tf (24) or fe (48), handle it accordingle
+
+    for flavour, value in selection.items():
+        # If the selection is false, continue
+        if not value:
+            continue
+        # If the key is 24/48 act accordingly
+        if flavour == 'tf':
+            import_df = pd.DataFrame.from_dict(different_24_dict)
+            string = import_df.to_csv(index=False)
+            
+        elif flavour == 'fe':
+            import_df = pd.DataFrame.from_dict(different_48_dict)
+            string = import_df.to_csv(index=False)
+        
+        else:
+            # Standard 96 well plate, add flavour to it
+            import_df = pd.DataFrame.from_dict(different_96_dict)
+            new_cols = [x if not x.startswith('Unnamed') else flavour for x in import_df.columns]
+            import_df.columns = new_cols
+
+            string = import_df.to_csv(index=False)
+
+        #Append string to sample sheet collective string + new line
+        sample_sheet += string + ',,,,,,,,,,,,\n'
+
+
+    # Add metadata to sample sheet
+    metadata = f"""Project Name,{project_name},,,,,,,,,,,\nExperiment Name,{experiment_name},,,,,,,,,,,\nComments,{additional_comments},,,,,,,,,,,\n"""
+    sample_sheet = metadata + sample_sheet
+
+    project_name = project_name.replace(' ', '_')
+    filename = f'template_{project_name}.{file_format}'
+
+    return filename, sample_sheet
+
+
+
+
+# Obtain information from the upload file and extract tables, reports and metadata
+def wrapper_handle_upload(content, filename):
+    """Obtain information from the upload file and extract tables, reports and metadata"""
+
+    #Checks before the function starts for filename and content
+    if filename is not None and filename.split('.')[1] != 'csv':
+        #return genericLabelWrapper("Please only upload 'csv' files"), None
+        return "Please only upload 'csv' files", None, None
+    if content is None:
+        # return genericLabelWrapper('The content of the file you uploaded is not valid'), None
+        return 'The content of the file you uploaded is not valid', None, None
+        
+    
+    # Handle file
+    # UNCOMMENT THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    content_type, content_string = content.split(',')
+    decoded_content = b64decode(content_string)
+    file_content = StringIO(decoded_content.decode('utf-8')).readlines()
+
+    # Only for debug
+    # file_content = content
+
+
+    # Slice metadata eg first 3 lines
+    metadata = file_content[:3]
+    # convert metadata
+    metadata_dict = get_metadata(metadata)
+
+    
+    # Get rest of file, read it in with pandas and get the tables
+    file_string = ''.join(file_content[3:])
+    # Read file in with pandas
+    df = pd.read_csv(StringIO(file_string), header=None)
+    # Helper function to extract the tables and return it in dictionary where flavour:df
+    tables = get_tables(df)
+
+    # Perform checks on tables
+    checks_tables = {key:check_table(value) for key, value in tables.items()}
+
+    return tables, checks_tables, metadata_dict
+
+
+
+
+
+
+# Get the complete sample sheet
+def wrapper_create_complete_sample_sheet(tables, complete, machinery, project, experiment, comments):
+    # Handle the presence of 24/48 plates
+    tf_or_fe = None
+    # Need to flag if the reverse complement is required
+    reverse_complement = False
+
+    # Retain 24/48 well layouts and produce the sample sheet for the rest
+    if 'tf' in tables.keys():
+        tf_or_fe = ('tf', tables['tf']) 
+        del tables['tf']
+    if 'fe' in tables.keys():
+        tf_or_fe = ('fe', tables['fe']) 
+        del tables['fe']
+
+    # Get body of sample sheet
+    sample_sheet_body = get_sample_sheet_body(tables, complete)
+
+
+    # Make header and adapt file based on machine chosen
+    header = None
+    filename=None
+    # 
+    if machinery == 'hi_seq':
+        # The header is basically empty
+        header = make_hiseq_header()
+        filename = f'{project}_SR_HS{"_all_primers" if complete else ""}.csv'
+        # File does not change
+    #
+    elif machinery == 'mini_seq':
+        # Header only has experiment 
+        header = make_miniseq_header(experiment)
+        # Refactor the columns to reduce them to the required number
+        sheet = sheet[['Sample_ID', 'Description', 'I7_Index_ID', 'index', 'I5_Index_ID', 'index2', 'Sample_Project']]
+        filename = f'{project}_MiniSeq{"_all_primers" if complete else ""}.csv'
+    #
+    elif machinery == 'mi_seq':
+        # Nothing special
+        header = make_miseq_header(project, experiment, comments)
+        filename = f'{project}_SR_MS{"_all_primers" if complete else ""}.csv'
+    #
+    elif machinery == 'next_seq':
+        header = make_nextseq_header(project, experiment, comments)
+        # Next seq requires reverse I5, map the index names to the correct sequence from the dictionary
+        sample_sheet_body['index2'] = sample_sheet_body['I5_Index_ID'].map(f_primers_to_reverse_complement)
+        reverse_complement = True
+        filename = f'{project}_PE_NS{"_all_primers" if complete else ""}.csv'
+    else:
+        raise Exception('I do not recognise this machinery instrument!')
+
+    # Append 24/48 plate to it
+    if tf_or_fe:
+        sample_sheet_body = add_tf_fe(sample_sheet_body, tf_or_fe, reverse_complement)
+
+    text = header + sample_sheet_body.to_csv(index=False, header=False)
+
+    return filename, text
+
+
+
+
+
